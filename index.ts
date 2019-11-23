8afd21cd72730af4bca00a9d71463bf082498354
izio38 <romainbillot3009@gmail.com>
2019-11-23 16:31:19 +0100

    fix( types declaration )

--- index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./src/discord.module";
-export * from "./src/discord.providers";
-export * from "./src/interaces/discord-module-register-options";
-export * from "./src/types/client-event";
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./discord.module";
+export * from "./discord.providers";
+export * from "./interaces/discord-module-register-options";
+export * from "./types/client-event";
