52c5e48d166f010469acaf630f1cf3c5d393681b
izio38 <romainbillot3009@gmail.com>
2019-11-23 15:59:57 +0100

    export all from index.ts

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from "./src/discord.module";
+export * from "./src/discord.providers";
+export * from "./src/interaces/discord-module-register-options";
+export * from "./src/types/client-event";
